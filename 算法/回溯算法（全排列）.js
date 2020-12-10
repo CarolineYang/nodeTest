ee332d00d094ab698e8866f6ba67fef31f2d0ca6
ypp <yang.pingping@tsing-intelligence.com>
2020-12-10 22:23:14 +0800

    tinymce

--- 算法/回溯算法（全排列）.js
+++ 算法/回溯算法（全排列）.js
@@ -5,5 +5,22 @@
 // 77. 组合
 // 78. 子集
 // 90. 子集 II
-var a = new Array(5);
-console.log(a)
+// var a = new Array(5);
+// console.log(a)
+var lengthOfLongestSubstring = function(s) {
+    let r;
+    let res = 0;
+    for(let i=0;i<s.length;i++){
+        r = i+1;
+        console.log(s.substring(i,r))
+        while(s.substring(i,r).indexOf(s[r])==-1){
+            r++;
+        }
+        if(res<r-i){
+            res = r-i;
+        }
+        i = r -1;
+    }
+    return res;
+};
+lengthOfLongestSubstring('abcabcbb')
