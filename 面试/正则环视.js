e56cf28f73e0c82290ffed28b5cb362a36320210
yangpingping <yang.pingping@tsing-intelligence.com>
2021-01-15 18:08:10 +0800

    Merge branch 'feature-test-ypp' of https://github.com/CarolineYang/nodeTest into feature-test-ypp
    # Conflicts:
    #	.idea/workspace.xml

--- 面试/正则环视.js
+++ 面试/正则环视.js
@@ -1,3 +1,4 @@
 let  a= '2 apple cost 30$45';
 let reg = /\d(?=)\d+/;
 console.log(reg.exec(a),a.match(reg))
+//?= ?!
--- 面试/继承.js
+++ 面试/继承.js
@@ -10,3 +10,31 @@ var b = new pp();
 a.a =20;
 a.b.push(1)
 console.log(b.a,a.a,a.b,b.b)
+class Person{
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+    }
+    sayhi(){
+        console.log(this.name,'hellp',this.age)
+    }
+}
+// let a = new Person('ypp',28);
+// console.log(a)
+// a.sayhi()
+class Child extends Person{
+    constructor(name,age,color){
+
+        super(name,age);
+        super.a = 10;
+
+        this.color = color;
+    }
+    sayhi() {
+        console.log(this.name,this.age,this.a,super.a)
+        super.sayhi();
+
+    }
+}
+var b = new Child('sss',20,'red')
+b.sayhi()
