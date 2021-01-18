86f0470ce395cca7711dd6f11d5192b8062d00e8
yangpingping <yang.pingping@tsing-intelligence.com>
2021-01-18 14:18:12 +0800

    Merge branch 'feature-test-ypp' of https://github.com/CarolineYang/nodeTest into feature-test-ypp
    # Conflicts:
    #	.idea/workspace.xml

--- a.js
+++ a.js
@@ -5,5 +5,8 @@ module.exports.haha=function () {
 }
 
 console.log('kkkk')
-console.log(module)
+// console.log(module)
 //exports 就是模块文件对外暴露开放的api （一个js就是一个模块）
+let a = "[ 0  , 17  ]"
+a = a.split(',')
+console.log(/\d+/.exec(a[1])[0],/\d+/.exec(a[0]))
